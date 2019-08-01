8ace895c4740e9a5b4ac112014576c250c14e629
Douglas Martin <mdgeek1@gmail.com>
2019-08-01 12:24:02 -0400

    Initial import

---
+++ fcf-mockepic-common/src/main/webjar/js/fcf-epic.js
@@ -0,0 +1,303 @@
+'use strict';
+
+define('fcf-epic', [], function() {
+
+    var AglAction = {
+        /**
+         * Performs the initial handshake with the AGL web service, returning an access token to accompany future requests.
+         */
+        INITIAL_HANDSHAKE: "Epic.Clinical.Informatics.Web.InitiateHandshake",
+        /**
+         * Places an order in the Epic EMR.
+         */
+        POST_ORDER: "Epic.Clinical.Informatics.Web.PostOrder",
+        /**
+         * Removes an order which triggered search.
+         */
+        REMOVE_TRIGGER_ORDER: "Epic.Clinical.Informatics.Web.RemoveTriggerOrder",
+        /**
+         * Removes order(s) in BPA web service workflow.
+         */
+        REMOVE_SPECIFIED_ORDER: "Epic.Clinical.Informatics.Web.RemoveSpecifiedOrder",
+        /**
+         * Replaces one order with another.
+         */
+        REPLACE_UNSIGNED_ORDER: "Epic.Clinical.Informatics.Web.ReplaceUnsignedOrder",
+        /**
+         * Posts a value to a flowsheet row.
+         */
+        POST_FLOWSHEET_ROW: "Epic.Clinical.Informatics.Web.PostFlowsheetRow",
+        /**
+         * Opens an external browser window.
+         */
+        OPEN_EXTERNAL_WINDOW: "Epic.Clinical.Informatics.Web.OpenExternalWindow",
+        /**
+         * Closes the current AGL activity.
+         */
+        CLOSE_ACTIVITY: "Epic.Clinical.Informatics.Web.CloseActivity",
+        /**
+         * Closes the current activity and aborts the signing process.
+         */
+        CLOSE_AND_CANCEL: "Epic.Clinical.Informatics.Web.CloseAndCancel",
+        /**
+         * Closes the current activity and continues the signing process.
+         */
+        CLOSE_AND_CONTINUE: "Epic.Clinical.Informatics.Web.CloseAndContinue",
+        /**
+         * Saves some state in case of hibernation.
+         */
+        SAVE_STATE: "Epic.Clinical.Informatics.Web.SaveState",
+        /**
+         * Launches an Epic activity (Epic 2018+).
+         */
+        LAUNCH_ACTIVITY: "Epic.Clinical.Informatics.Web.LaunchActivity"
+    };
+
+    var agl_sessions = [];
+
+    var okResponse = {actionExecuted: true, errorCodes: []};
+
+    function messageListener(event) {
+        var request = event.data;
+
+        if (!request || !request.action || !event.source) {
+            return;
+        }
+
+        var session = getSession(event.source);
+
+        console.log('Received AGL Request: ' + JSON.stringify(request, null, 2));
+        var response;
+
+        switch (request.action) {
+            case AglAction.INITIAL_HANDSHAKE:
+                response = initialHandshake(request, session);
+                break;
+
+            case AglAction.POST_ORDER:
+                response = validate(request, session) || postOrder(request, session);
+                break;
+
+            case AglAction.REMOVE_TRIGGER_ORDER:
+                response = validate(request, session) || removeTriggerOrder(request, session);
+                break;
+
+            case AglAction.REMOVE_SPECIFIED_ORDER:
+                response = validate(request, session) || removeSpecifiedOrder(request, session);
+                break;
+
+            case AglAction.REPLACE_UNSIGNED_ORDER:
+                response = validate(request, session) || replaceUnsignedOrder(request, session);
+                break;
+
+            case AglAction.POST_FLOWSHEET_ROW:
+                response = validate(request, session) || postFlowsheetRow(request, session);
+                break;
+
+            case AglAction.OPEN_EXTERNAL_WINDOW:
+                response = validate(request, session) || openExternalWindow(request, session);
+                break;
+
+            case AglAction.CLOSE_ACTIVITY:
+                response = validate(request, session) || closeActivity(request, session);
+                break;
+
+            case AglAction.CLOSE_AND_CANCEL:
+                response = validate(request, session) || closeAndCancel(request, session);
+                break;
+
+            case AglAction.CLOSE_AND_CONTINUE:
+                response = validate(request, session) || closeAndContinue(request, session);
+                break;
+
+            case AglAction.SAVE_STATE:
+                response = validate(request, session) || saveState(request, session);
+                break;
+
+            case AglAction.LAUNCH_ACTIVITY:
+                response = validate(request, session) || launchActivity(request, session);
+                break;
+
+            default:
+                response = badRequest(request, session);
+        }
+
+        response.errorCodes = response.errorCodes || [];
+        console.log('Sent AGL Response: ' + JSON.stringify(response, null, 2));
+        sendResponse(response, session);
+    }
+
+    function getSession(source) {
+        var session = _.find(agl_sessions, function (s) {
+            return s.source === source;
+        });
+
+        if (!session) {
+            var header = $('<div></div>').hide().insertBefore(getIFrame(source));
+            session = {source: source, cart: [], header: header};
+            agl_sessions.push(session);
+        }
+
+        return session;
+    }
+
+    function updateHeader(session) {
+        session.cart.length ? session.header.show() : session.header.hide();
+        session.header.text('Items in cart: ' + session.cart.length);
+    }
+
+    function initialHandshake(request, session) {
+        session.token = createToken();
+        session.cart = [];
+        session.header.hide();
+
+        return {
+            actionExecuted: true,
+            token: session.token,
+            actions: _.values(AglAction),
+            version: '10.0',
+            state: session.state
+        }
+    }
+
+    function validate(request, session) {
+        if (request.token == null) {
+            return badRequest(request, 'No token in request.');
+        } else if (request.token !== session.token) {
+            return badRequest(request, 'Bad AGL token: ' + request.token)
+        } else {
+            return null;
+        }
+    }
+
+    function postOrder(request, session) {
+        if (!request.args || request.args.OrderKey == null) {
+            return badRequest(request, 'No order key in request.', [0]);
+        } else {
+            session.cart.push('Order for key ' + request.args.OrderKey);
+            updateHeader(session);
+            return okResponse;
+        }
+    }
+
+    function removeTriggerOrder(request) {
+        if (request.args) {
+            return badRequest(request, request.action + ' requires no arguments.  The following arguments were provided: ' + request.args);
+        } else {
+            return okResponse;
+        }
+    }
+
+    function removeSpecifiedOrder(request) {
+        if (typeof request.args !== 'string') {
+            return badRequest(request, 'Bad or missing arguments provided for ' + request.action);
+        } else {
+            return okResponse;
+        }
+    }
+
+    function replaceUnsignedOrder(request) {
+        if (!request.args || _.keys(request.args).length === 0) {
+            return badRequest(request, 'No arguments provided for ' + request.action);
+        } else {
+            return okResponse;
+        }
+    }
+
+    function postFlowsheetRow(request) {
+        if (!request.args || request.args.RowValue == null) {
+            return badRequest(request, request.action + ' requires a RowValue.');
+        } else {
+            return okResponse;
+        }
+    }
+
+    function openExternalWindow(request) {
+        if (typeof request.args !== 'string' && !Array.isArray(request.args)) {
+            return badRequest(request, request.action + ' requires a destination URL.');
+        } else {
+            var args = Array.isArray(request.args) ? request.args : [request.args]
+            window.open.apply(window, args);
+            return okResponse;
+        }
+    }
+
+    function closeActivity(request, session) {
+        return close(request, session);
+    }
+
+    function closeAndCancel(request, session) {
+        return close(request, session);
+    }
+
+    function closeAndContinue(request, session) {
+        return close(request, session);
+    }
+
+    function close(request, session) {
+        session.token = null;
+        session.state = null;
+        session.cart = [];
+        updateHeader(session);
+        var iframe = getIFrame(session.source);
+
+        if (iframe) {
+            iframe.src = 'about:blank';
+            return okResponse;
+        } else {
+            return badRequest(request, 'No iframe found for the request.')
+        }
+    }
+
+    function getIFrame(source) {
+        return _.find(window.document.getElementsByTagName('iframe'), function(iframe) {
+            return iframe.contentWindow === source;
+        });
+    }
+
+    function saveState(request, session) {
+        session.state = request.args;
+        return okResponse;
+    }
+
+    function launchActivity(request) {
+        if (!request.args || request.args.ActivityKey == null) {
+            return badRequest(request, request.action + ' requires an ActivityKey.')
+        } else {
+            alert('Received AGL launch activity request: ' + request.args.ActivityKey);
+            return okResponse
+        }
+    }
+
+    /**
+     * For requests that are unrecognized or invalid.
+     *
+     * @param request The AGL request.
+     * @param error Optional error text.  If not provided, default error text will be used.
+     * @param errorCodes Optional array of error codes.
+     */
+    function badRequest(request, error, errorCodes) {
+        return {
+            actionExecuted: false,
+            error: error || 'Bad request: ' + JSON.stringify(request, null, 2),
+            errorCodes: errorCodes
+        };
+    }
+
+    function createToken() {
+        return (Math.random() * 1e32).toString(36);
+    }
+
+    /**
+     * Send a response back to the iframe window.
+     *
+     * @param response The AGL response.
+     */
+    function sendResponse(response, session) {
+        if (response) {
+            session.source.postMessage(response, '*');
+        }
+    }
+
+    window.addEventListener('message', messageListener, false);
+});
---
+++ fcf-mockepic-dstu2/src/main/java/org/fujionclinical/epic/dstu2/SmartContextEncounter.java
@@ -0,0 +1,62 @@
+/*
+ * #%L
+ * Fujion Clinical Framework
+ * %%
+ * Copyright (C) 2018 fujionclinical.org
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * This Source Code Form is also subject to the terms of the Health-Related
+ * Additional Disclaimer of Warranty and Limitation of Liability available at
+ *
+ *      http://www.fujionclinical.org/licensing/disclaimer
+ *
+ * #L%
+ */
+package org.fujionclinical.epic.dstu2;
+
+import ca.uhn.fhir.model.dstu2.composite.IdentifierDt;
+import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import org.fujionclinical.fhir.dstu2.api.common.FhirUtil;
+import org.fujionclinical.fhir.dstu2.api.encounter.EncounterContext;
+import org.fujionclinical.fhir.smart.common.SmartContextBase;
+import org.fujionclinical.epic.common.core.Constants;
+
+/**
+ * Implements SMART context scope to supply CSN in SMART context.
+ */
+public class SmartContextEncounter extends SmartContextBase {
+
+    /**
+     * Binds encounter context changes to the SMART encounter context scope.
+     */
+    public SmartContextEncounter() {
+        super("encounter.epic", "CONTEXT.CHANGED.Encounter");
+    }
+
+    /**
+     * Populate context map with information about currently selected encounter.
+     *
+     * @param context Context map to be populated.
+     */
+    @Override
+    protected void updateContext(ContextMap context) {
+        Encounter encounter = EncounterContext.getActiveEncounter();
+        IdentifierDt id = encounter == null ? null : FhirUtil.getIdentifierBySystem(encounter.getIdentifier(), Constants.CSN_SYSTEM);
+
+        if (id != null) {
+            context.put("csn", id.getValue());
+        }
+    }
+
+}
