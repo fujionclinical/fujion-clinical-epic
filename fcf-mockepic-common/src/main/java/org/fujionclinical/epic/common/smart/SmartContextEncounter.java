c792167ae7679b6f721fde2544e5d448ab464d0b
Douglas Martin <mdgeek1@gmail.com>
2021-12-06 18:42:55 -0500

    Accommodate changes to COOL model.

--- fcf-mockepic-common/src/main/java/org/fujionclinical/epic/common/smart/SmartContextEncounter.java
+++ fcf-mockepic-common/src/main/java/org/fujionclinical/epic/common/smart/SmartContextEncounter.java
@@ -26,7 +26,7 @@
 package org.fujionclinical.epic.common.smart;
 
 import org.coolmodel.clinical.encounter.Encounter;
-import org.coolmodel.core.datatype.Identifier;
+import org.coolmodel.core.complex.Identifier;
 import org.fujionclinical.api.cool.encounter.EncounterContext;
 import org.fujionclinical.epic.common.core.Constants;
 import org.fujionclinical.fhir.smart.common.SmartContextBase;
