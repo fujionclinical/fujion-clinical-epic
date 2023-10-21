14740131925c2eee4738759c02235163c876e9d1
Douglas Martin <mdgeek1@gmail.com>
2023-10-21 12:21:41 -0400

    Java 17 conversion.

--- fcf-mockepic-service/src/main/java/org/fujionclinical/epic/service/IMockEpicService.java
+++ fcf-mockepic-service/src/main/java/org/fujionclinical/epic/service/IMockEpicService.java
@@ -25,8 +25,8 @@
  */
 package org.fujionclinical.epic.service;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
